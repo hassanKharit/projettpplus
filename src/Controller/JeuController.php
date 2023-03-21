6ab1bad82fd871842a1db59ac637477f1b76c459
kharit <hasjok@msn.com>
2023-03-21 11:36:45 +0100

    first commit

---
+++ src/Controller/JeuController.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Controller;
+
+use App\Form\JeuType;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class JeuController extends AbstractController
+{
+    #[Route('/jeu', name: 'app_jeu')]
+    public function index(Request $request): Response
+    {
+        $form=$this->createForm(JeuType::class);
+
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid())
+        {
+            $data=$form->getData();
+
+            $data['alea']=rand(0,100);
+
+            if($data['alea']==$data['number']){
+                $data['reponse']="vous avez gagner";
+            }else{
+                $data['reponse']="vous avez perdu";
+            }
+
+            return $this->render('jeu/traitement.html.twig', [
+                'jeu'=>$data,
+            ]);
+
+        }
+        return $this->renderForm('jeu/index.html.twig', [
+            'form' => $form,
+        ]);
+    }
+}
